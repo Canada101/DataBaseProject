101085f658741adface979915ef570a4056478fb
hnaismith <henry.naismith@slu.edu>
2024-11-23 10:13:37 -0600

    Adding the display table

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
+import pandas as pd
 from config import MYSQL_CONFIG
 
 app = Flask(__name__)
@@ -41,6 +42,26 @@ def remove_part():
     cursor.close()
     connection.close()
     return redirect(url_for('index'))
+
+@app.route('/show_table', methods=['POST'])
+def show_table():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM parts')
+    myresults = cursor.fetchall()
+    connection.commit()
+    cursor.close()
+    connection.close()
+    
+    # Create DataFrame and convert to HTML
+    df = pd.DataFrame(myresults)
+    table_html = df.to_html()
+    
+    return render_template('data_table.html', data_table=table_html)
+
+@app.route('/return_to_add', methods=['POST'])
+def return_to_add():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
